e8a9d7dba6e62e9c15d59a3bccfa543c196dcb46
QuaidShubin <quaid.shubin@gmail.com>
2025-06-29 01:33:42 -0700

    verbose? i dont think this is very verbose? do you?

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import os
 from dotenv import load_dotenv
 import sys
+from google import genai
+from google.genai import types
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     print("Expected 2 arguments")
     sys.exit(1)
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-from google import genai
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
 
 client = genai.Client(api_key=api_key)
-
-prompt = sys.argv[1]
-
 response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=prompt
+    model="gemini-2.0-flash-001",
+    contents=messages,
 )
+
 print(response.text)
-print(f'''Prompt tokens: {response.usage_metadata.prompt_token_count}
-Response tokens: {response.usage_metadata.candidates_token_count}''')
+
+verbose = "--verbose" in sys.argv[2:]
+
+if verbose:
+    print(
+        f"""\
+User prompt: {user_prompt}
+Prompt tokens: {response.usage_metadata.prompt_token_count}
+Response tokens: {response.usage_metadata.candidates_token_count}"""
+    )
