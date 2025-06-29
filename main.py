eee4a5531011a08876fb79e8180de4ad231aa372
QuaidShubin <quaid.shubin@gmail.com>
2025-06-29 00:26:10 -0700

    choose ur adventure

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
 from dotenv import load_dotenv
+import sys
+
+if len(sys.argv) != 2:
+    print("Expected 2 arguments")
+    sys.exit(1)
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,8 +13,10 @@ from google import genai
 
 client = genai.Client(api_key=api_key)
 
+prompt = sys.argv[1]
+
 response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.0-flash-001', contents=prompt
 )
 print(response.text)
 print(f'''Prompt tokens: {response.usage_metadata.prompt_token_count}
