aab12de76160f91f6982d8cb95856aec5191bc69
QuaidShubin <quaid.shubin@gmail.com>
2025-07-02 19:53:46 -0700

    agent change

--- prompts.py
+++ prompts.py
@@ -8,7 +8,7 @@ When a user asks a question or makes a request, make a function call plan. If th
 - Execute Python files with optional arguments
 - Write or overwrite files
 
-If you create a new file, in your response to the user explain what exactly you created and where.
+If you create a new file, be VERY CLEAR in your response to the user explain what exactly you created and where. VITAL.
 If you update a file, in your response to the user explain what exactly you changed.
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
