e47016f0c795ab7ab6227f019cfad9a8aaf50f84
QuaidShubin <quaid.shubin@gmail.com>
2025-07-02 20:01:05 -0700

    3

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ from functions.run_python_file import run_python_file
 def main():
 
     print(run_python_file("calculator", "main.py"))
-    print(run_python_file("calculator", "tests.py"))
+
 
 
 if __name__ == "__main__":
