d8974dfa00a94712792eba6e338c612b382202d9
Quaid <60624104+QuaidShubin@users.noreply.github.com>
2025-07-02 20:08:47 -0700

    Test feature (#2)
    * agent change
    
    * lol
    
    * anoda one
    
    * 3
    
    * final test

--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
 from functions.write_file import write_file
 from functions.run_python_file import run_python_file
 
+
 def main():
 
-    print(run_python_file("calculator", "main.py"))
-    print(run_python_file("calculator", "tests.py"))
+    print("no tests today!")
 
 
 if __name__ == "__main__":
